024ec1926b61fba549e122e3a379b866ca7705af
xeusteerapat <xeus085@gmail.com>
2020-02-18 21:35:08 +0700

    change function name in algojs

--- algo.js
+++ algo.js
@@ -1,143 +1,143 @@
 // ข้อ 1
-// function getAsterix(n) {
-//   let ans = "";
-//   for (let i = 1; i <= n; i++) {
-//     ans += "*";
-//   }
-//   console.log(ans);
-// }
+function func1(n) {
+  let ans = "";
+  for (let i = 1; i <= n; i++) {
+    ans += "*";
+  }
+  console.log(ans);
+}
 
-// getAsterix(5);
+func1(5);
 
 // ข้อ 2
-// function getAsterix(n) {
-//   let ans = "";
-//   for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= n; j++) {
-//       ans += "*";
-//     }
-//     ans += "\n";
-//   }
-//   console.log(ans);
-// }
+function func2(n) {
+  let ans = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      ans += "*";
+    }
+    ans += "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(6);
+func2(6);
 
 // ข้อ 3
-// function getAsterix(n) {
-//   let ans = "";
-//   for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= n; j++) {
-//       ans += j;
-//     }
-//     ans += "\n";
-//   }
-//   console.log(ans);
-// }
+function func3(n) {
+  let ans = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      ans += j;
+    }
+    ans += "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(4);
+func3(4);
 
 // ข้อ 4
-// function getAsterix(n) {
-//   let ans = "";
-//   for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= n; j++) {
-//       ans += i;
-//     }
-//     ans += "\n";
-//   }
-//   console.log(ans);
-// }
+function func4(n) {
+  let ans = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      ans += i;
+    }
+    ans += "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(4);
+func4(4);
 
 // ข้อ 5
-// function getAsterix(n) {
-//   let ans = "";
-//   for (let i = n; i >= 1; i--) {
-//     for (let j = n; j >= 1; j--) {
-//       ans += i;
-//     }
-//     ans += "\n";
-//   }
-//   console.log(ans);
-// }
+function func5(n) {
+  let ans = "";
+  for (let i = n; i >= 1; i--) {
+    for (let j = n; j >= 1; j--) {
+      ans += i;
+    }
+    ans += "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(4);
+func5(4);
 
 // ข้อ 6
-// function getAsterix(n) {
-//   let ans = "";
-//   let k = 1;
-//   for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= n; j++) {
-//       ans += k;
-//       k++;
-//     }
-//     ans += "\n";
-//   }
-//   console.log(ans);
-// }
+function func6(n) {
+  let ans = "";
+  let k = 1;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      ans += k;
+      k++;
+    }
+    ans += "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(5);
+func6(5);
 
 // ข้อ 7
-// function getAsterix(n) {
-//   let ans = "";
-//   let k = n * n;
-//   for (let i = n; i >= 1; i--) {
-//     for (let j = n; j >= 1; j--) {
-//       ans += k;
-//       k--;
-//     }
-//     ans += "\n";
-//   }
-//   console.log(ans);
-// }
+function func7(n) {
+  let ans = "";
+  let k = n * n;
+  for (let i = n; i >= 1; i--) {
+    for (let j = n; j >= 1; j--) {
+      ans += k;
+      k--;
+    }
+    ans += "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(4);
+func7(4);
 
 // ข้อ 8
-// function getAsterix(n) {
-//   let ans = "";
-//   for (let i = 0; i <= n + 2; i += 2) {
-//     ans += i + "\n";
-//   }
-//   console.log(ans);
-// }
+function func8(n) {
+  let ans = "";
+  for (let i = 0; i <= n + 2; i += 2) {
+    ans += i + "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(3);
+func8(3);
 
 // ข้อ 9
-// function getAsterix(n) {
-//   let ans = "";
-//   for (let i = 2; i <= 2 * n; i += 2) {
-//     ans += i + "\n";
-//   }
-//   console.log(ans);
-// }
+function func9(n) {
+  let ans = "";
+  for (let i = 2; i <= 2 * n; i += 2) {
+    ans += i + "\n";
+  }
+  console.log(ans);
+}
 
-// getAsterix(4);
+func9(4);
 
 // ข้อ 10
-// function getAsterix(n) {
-//   let ans = "";
-//   let k = 1;
-//   for (let i = 1; i <= n; i++) {
-//     for (let j = k; j <= n * k; j += k) {
-//       ans += j;
-//     }
-//     ans += "\n";
-//     k++;
-//   }
+function func10(n) {
+  let ans = "";
+  let k = 1;
+  for (let i = 1; i <= n; i++) {
+    for (let j = k; j <= n * k; j += k) {
+      ans += j;
+    }
+    ans += "\n";
+    k++;
+  }
 
-//   console.log(ans);
-// }
+  console.log(ans);
+}
 
-// getAsterix(3);
+func10(3);
 
 // ข้อ 11
-function getAsterix(n) {
+function func11(n) {
   let ans = "";
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
@@ -151,7 +151,7 @@ function getAsterix(n) {
   }
   console.log(ans);
 }
-getAsterix(4);
+func11(4);
 
 // ข้อ 12
 // function getAsterix(n) {
